e9bd4f9763662e7ca758b8b61c31fb40d0fe5aa0
Lauren Yu <laurenkyu@gmail.com>
2020-01-20 15:07:40 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -54,7 +54,7 @@ end
 def reduce_to_all_true(array)
   index = 0 
   while index < array.length
-    if reduce_to_all_true[index] = false 
+    if !reduce_to_all_true[index]
       return false
     end 
     index += 1 
